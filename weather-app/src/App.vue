0859027bad0b308dd633a4d58390713310510cdb
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2022-10-19 19:53:46 -0300

    applied code formatting

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <weatherApp/>
+    <weatherApp />
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import weatherApp from './components/weather-app.vue'
-export default defineComponent( {
- name: 'App',
- components: {
-  weatherApp
- }
+export default defineComponent({
+  name: 'App',
+  components: {
+    weatherApp
+  }
 });
 </script>
 <style>
+
 </style>
