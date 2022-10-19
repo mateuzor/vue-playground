2a77065ac0fd1b2ee237e4c06d7de6a779b1df13
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2022-10-19 18:08:48 -0300

    weather finished

---
+++ weather-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <weatherApp/>
+  </div>
+</template>
+<script>
+import weatherApp from './components/weather-app.vue'
+export default {
+ name: 'App',
+ components: {
+  weatherApp
+ }
+}
+</script>
+<style>
+</style>
