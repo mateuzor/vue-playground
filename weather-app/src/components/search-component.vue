868fb46e7c4c74d3a7814edb134c9cbdd9eed2c6
Mateus Ramos Ferreira <mateuzor@Mateuss-MacBook-Pro.local>
2022-10-26 22:17:16 -0300

    refactored search component to composition api

--- weather-app/src/components/search-component.vue
+++ weather-app/src/components/search-component.vue
@@ -4,35 +4,26 @@
     </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import axios from "axios";
-import { defineComponent } from 'vue'
 import WeatherType from '../types/Weather'
-export default defineComponent({
-    name: 'SearchComponent',
-    data() {
-        return {
-            api_key: "28953c7f3c1d3e66c812efd326e55f9d",
-            url_base: "https://api.openweathermap.org/data/2.5/",
-            weather_icon: "http://openweathermap.org/img/wn/",
-            query: "",
-            weather: {} as WeatherType
-        };
-    },
-    methods: {
-        async fetchWeather(e: KeyboardEvent) {
-            if (e.key == "Enter") {
-                let response = await axios.get(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`);
-                this.setResults(response.data);
+import {defineEmits, ref} from 'vue'
+const api_key = ref("28953c7f3c1d3e66c812efd326e55f9d")
+const url_base = ref("https://api.openweathermap.org/data/2.5/")
+const query = ref("")
+let weather = ref({} as WeatherType)
+const emit = defineEmits(['weather'])
+const fetchWeather = (async (e: KeyboardEvent) => {
+    if (e.key == "Enter") {
+                let response = await axios.get(`${url_base.value}weather?q=${query.value}&units=metric&APPID=${api_key.value}`);
+                setResults(response.data);
             }
-        },
-        setResults(returnedResponse: WeatherType) {
-            console.log(returnedResponse);
-            this.weather = returnedResponse;
-            this.$emit('weather', this.weather);
-        },
-    },
-});
+})
+const setResults =  (returnedResponse: WeatherType)  =>{
+    console.log(returnedResponse);
+            weather = ref(returnedResponse);
+            emit('weather', weather.value);   
+}
 </script>
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;700;900&display=swap");
